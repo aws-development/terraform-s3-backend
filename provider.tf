4d00a0c61d7964ba531678bae3bc81f127963629
Ravi Kumar <RjDev@users.noreply.github.com>
2023-04-15 07:24:56 +0000

    creating s3 backend bucket

--- provider.tf
+++ provider.tf
@@ -29,3 +29,24 @@ provider "aws" {
   }
 }
 
+
+# Configure the AWS Provider
+provider "aws" {
+  alias = "replica"
+  region     = "us-west-1"
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+
+
+  default_tags {
+    tags = {
+      env         = var.env
+      provider    = var.tf_provider
+      platform    = var.platform
+      application = var.application
+      cost-center = var.cost_center
+      owner       = var.owner
+      lob         = var.lob
+    }
+  }
+}
