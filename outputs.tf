c0543f9b08341d5eff0b277e71a9cfbe365e8b29
Ravi Kumar <RjDev@users.noreply.github.com>
2023-04-17 17:57:57 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,9 @@ output "state_bucket" {
   description = "The S3 bucket to store the remote state file."
   value       = module.remote_state.state_bucket.bucket
 }
+output "dynamo_db_table_id" {
+  description = "dynamo db table id"
+  value       = module.remote_state.dynamodb_table.id
+}
+
+
